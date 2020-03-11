3b522b498b1b3bc6f562a303f4513335800a729c
Cem Aydemir <cem.aydemir@student.pxl.be>
2020-03-11 15:58:56 +0100

    v1

---
+++ src/components/BookList.vue
@@ -0,0 +1,16 @@
+<template>
+      <div id='book-list'>
+          <h1>{{title}}</h1>
+      </div>
+</template>
+<style>
+h1,h2 {font-weight:normal ;}
+</style>
+<script>
+        new Vue({
+          el: '#book-list',
+          data: {
+              title: 'All books'
+          }
+       })
+</script>
